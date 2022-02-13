e4bc50df7f0a6c313e8d643bed1667f33ef75887
KarateSwagg22 <mikail.doumi@gmail.com>
2022-02-13 11:09:30 +0100

    init

---
+++ Code Courses/course.js
@@ -0,0 +1,9 @@
+//COURS DE JAVASCRIPT
+
+// Liste events : https://www.khanacademy.org/computing/computer-programming/html-css-js/html-js-dom-events/a/dom-event-types
+
+var value=10;
+document.getElementById('id').innerHTML = value;
+document.getElementById('id').addEventListener("click",function(){
+    alert('Je pue des fesses.')
+});
