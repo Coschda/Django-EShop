4f282c8b1749493a9251142ee91066fbad52018f
KarateSwagg22 <mikail.doumi@gmail.com>
2022-02-13 11:40:56 +0100

    Subscribe button désactive si caractère spécial.

--- Projet Django/Django/static/js/newsletter.js
+++ Projet Django/Django/static/js/newsletter.js
@@ -3,19 +3,22 @@ var defaut = "#ced4da";
 
 function EmailCheck(mail){
     var box = document.getElementById('form5Example21');
+    var button = document.getElementById('email-button');
     var issueArr = [];
 
     if (box.value.length<1)
         issueArr = [];
         box.style.outline = 'none';
+        button.disabled = true;
 
     if (/([^\w!\&+@-Z.]+)/.test(mail)){
         box.style.outline = '2px solid #8C1010';
         issueArr.push("Pas de caractères spéciaux !");
-        
+        button.disabled = true;
     }else{
         issueArr = [];
         box.style.outline = 'none';
+        button.disabled = false;
     }
     document.getElementById('form5Example21label').innerHTML=issueArr;
 }
